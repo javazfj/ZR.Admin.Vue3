d6a1567eececef687976bee23ba31fd08c224b89
javazfj <javazfj@163.com>
2025-06-16 11:15:38 +0800

    安装包

--- src/App.vue
+++ src/App.vue
@@ -55,8 +55,8 @@ watch(
     immediate: true
   }
 )
-console.log('🎉源码地址: https://gitee.com/izory/ZrAdminNetCore')
+/* console.log('🎉源码地址: https://gitee.com/izory/ZrAdminNetCore')
 console.log('📖官方文档：http://www.izhaorui.cn')
 console.log('💰打赏作者：http://www.izhaorui.cn/doc/support.html')
-console.log('📱移动端体验：http://www.izhaorui.cn/h5')
+console.log('📱移动端体验：http://www.izhaorui.cn/h5') */
 </script>
